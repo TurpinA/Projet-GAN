c80f20f06a8c16ae2609b72318b124b2466111cb
Turpin Alexandre <58237796+TurpinA@users.noreply.github.com>
2020-01-07 14:00:48 +0100

    Update art_gan.py

--- art_gan.py
+++ art_gan.py
@@ -23,7 +23,7 @@ GENERATE_RES = 3
 IMAGE_SIZE = 128  # rows/cols
 IMAGE_CHANNELS = 3
 
-training_data = np.load('test_sillure_data.npy')
+training_data = np.load('data.npy')
 
 
 def build_discriminator(image_shape):
